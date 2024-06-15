74eba4d4fe66208202a486fa0051dd4d3f5a4541
janani anbarasan <jni@example.com>
2024-06-15 11:41:57 -0500

    move the model tests

--- tests/analysis_test.go
+++ tests/analysis_test.go
@@ -2,15 +2,77 @@ package tests
 
 import (
 	"testing"
-	"net/http"
-	"net/http/httptest"
-	"github.com/stretchr/testify/assert"
+	"time"
+
 	"github.com/aries-financial-inc/options-service/models"
-	"github.com/aries-financial-inc/options-service/controllers"
+	"github.com/stretchr/testify/assert"
+
+	"github.com/aries-financial-inc/options-service/errors"
 )
 
+// TODO: idiomatic way of writing tests in golang is to keep tests and code together. fix the folder structure
 func TestOptionsContractModelValidation(t *testing.T) {
-	// Your code here
+	t.Run("invalid options type", func(t *testing.T) {
+		assert.ErrorIs(t, models.OptionsContract{
+			OptionsType: 0,
+		}.IsValid(), errors.ErrInvalidOptionsType)
+	})
+
+	t.Run("invalid strike price", func(t *testing.T) {
+		assert.ErrorIs(t, models.OptionsContract{
+			OptionsType: models.CALL,
+		}.IsValid(), errors.ErrInvalidStrikePrice)
+	})
+
+	t.Run("invalid ask price", func(t *testing.T) {
+		assert.ErrorIs(t, models.OptionsContract{
+			OptionsType: models.CALL,
+			StrikePrice: 100.0,
+			BidPrice:    10.05,
+		}.IsValid(), errors.ErrInvalidAskPrice)
+
+		// ask price is smaller than bid price
+		assert.ErrorIs(t, models.OptionsContract{
+			OptionsType: models.CALL,
+			StrikePrice: 100.0,
+			AskPrice:    10.0,
+			BidPrice:    12.4,
+		}.IsValid(), errors.ErrAskBidMismatch)
+	})
+
+	t.Run("invalid expiration date", func(t *testing.T) {
+		assert.ErrorIs(t, models.OptionsContract{
+			OptionsType: models.CALL,
+			StrikePrice: 100.0,
+			BidPrice:    10.05,
+			AskPrice:    12.04,
+			Position:    models.LONG,
+		}.IsValid(), errors.ErrInvalidExpirationDate)
+
+		// expiration date in in the past
+		assert.ErrorIs(t, models.OptionsContract{
+			OptionsType:    models.CALL,
+			StrikePrice:    100.0,
+			BidPrice:       10.05,
+			AskPrice:       12.04,
+			Position:       models.LONG,
+			ExpirationDate: time.Now().Add(-24 * time.Hour),
+		}.IsValid(), errors.ErrInvalidExpirationDate)
+	})
+
+	t.Run("invalid options type", func(t *testing.T) {
+		expirationDate, err := time.Parse(time.RFC3339, "2025-12-17T00:00:00Z")
+		assert.NoError(t, err)
+		assert.NoError(t, models.OptionsContract{
+			OptionsType:    models.CALL,
+			StrikePrice:    100.0,
+			BidPrice:       10.05,
+			AskPrice:       12.04,
+			Position:       models.LONG,
+			ExpirationDate: expirationDate,
+		}.IsValid())
+	})
+
 }
 
 func TestAnalysisEndpoint(t *testing.T) {
