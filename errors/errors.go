31cbe4283374b1a66b595ff94b9b4ac0e4363f83
janani anbarasan <jni@example.com>
2024-06-15 11:27:32 -0500

    add options contract model

---
+++ errors/errors.go
@@ -0,0 +1,13 @@
+package errors
+
+import "errors"
+
+var (
+	ErrInvalidOptionsType    = errors.New("invalid option type")
+	ErrInvalidStrikePrice    = errors.New("invalid strike price")
+	ErrInvalidAskPrice       = errors.New("invalid ask price")
+	ErrInvalidBidPrice       = errors.New("invalid bid price")
+	ErrAskBidMismatch        = errors.New("ask price must be greater than bid price")
+	ErrInvalidExpirationDate = errors.New("invalid expiration date")
+	ErrInvalidPosition       = errors.New("invalid position")
+)
--- models/optionsContract.go
+++ models/optionsContract.go
@@ -1,3 +1,62 @@
 package models
 
-// Your model here
+import (
+	"time"
+
+	appErrors "github.com/aries-financial-inc/options-service/errors"
+)
+
+type optionsType int
+
+const (
+	CALL optionsType = iota + 1
+	PUT
+)
+
+type position int
+
+const (
+	LONG position = iota + 1
+	SHORT
+)
+
+type OptionsContract struct {
+	OptionsType    optionsType
+	StrikePrice    float64
+	BidPrice       float64
+	AskPrice       float64
+	ExpirationDate time.Time
+	Position       position
+}
+
+func (o OptionsContract) IsValid() error {
+	if o.OptionsType == 0 {
+		return appErrors.ErrInvalidOptionsType
+	}
+
+	if o.StrikePrice <= 0 {
+		return appErrors.ErrInvalidStrikePrice
+	}
+
+	if o.BidPrice <= 0 {
+		return appErrors.ErrInvalidBidPrice
+	}
+
+	if o.AskPrice <= 0 {
+		return appErrors.ErrInvalidAskPrice
+	}
+
+	if o.AskPrice < o.BidPrice {
+		return appErrors.ErrAskBidMismatch
+	}
+
+	if o.Position == 0 {
+		return appErrors.ErrInvalidPosition
+	}
+
+	if o.ExpirationDate.IsZero() || o.ExpirationDate.Before(time.Now()) {
+		return appErrors.ErrInvalidExpirationDate
+	}
+
+	return nil
+}
